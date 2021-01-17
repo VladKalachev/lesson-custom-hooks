2d75be4b26fd7cbb2bf224095c64b06f2d414bb5
Corvax <vlvete1990@yandex.ru>
2021-01-17 12:39:09 +0300

    chore: add useInput useFetch useTheme

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import useInput from './hooks/useInput'
+import useFetch from './hooks/useFetch'
+import useTheme from './hooks/useTheme'
 
 function App() {
+  const inp = useInput('Hello World', true)
+  const inp2 = useInput('Hello World', true)
+  
+  const { data, loading, error } = useFetch('https://www.reddit.com/r/news.json')
+
+  const { theme, toggleTheme } = useTheme()
+
+  if(loading) return 'Loading ...'
+
+  if(error) {
+    console.log('error', error);
+    return null
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`App ${theme}`}>
+      <button onClick={toggleTheme}>
+        Toggle Theme
+      </button>
+      <form>
+        <input {...inp} />
+          {inp.error && <span style={{ color: 'red' }}>{inp.error}</span>}
+          <input {...inp2} />
+          {inp2.error && <span style={{ color: 'red' }}>{inp2.error}</span>}
+      </form>
+      {JSON.stringify(data && data.dist)}
     </div>
   );
 }
